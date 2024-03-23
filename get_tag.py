37b45e6334118270137f347f284c5c12b4b63aae
HellLord <ratul.debnath.year@gmail.com>
2024-03-24 02:23:33 +0600

    undo repo

--- get_tag.py
+++ get_tag.py
@@ -152,7 +152,6 @@ def get_gh_commits(repository: str) -> list[str]:
     repository, base = _get_gh_repository_base(repository)
     repository, branch = _get_repository_branch(repository)
     repository, path = _get_repository_path(repository)
-    print(f"repo={repository}")
     url = f"{base}/repos/{repository}/commits?sha={branch}&path={path}"
     response = _urlopen(url)
     return [result["sha"] for result in reversed(json.loads(response.read()))]
@@ -164,7 +163,6 @@ def get_gh_commit(repository: str) -> str:
 
 def get_gh_tags(repository: str) -> list[str]:
     repository, base = _get_gh_repository_base(repository)
-    print(f"repo={repository}")
     url = f"{base}/repos/{repository}/tags"
     response = _urlopen(url)
     return [result["name"] for result in reversed(json.loads(response.read()))]
@@ -176,7 +174,6 @@ def get_gh_tag(repository: str) -> str:
 
 def get_gh_releases(repository: str) -> list[str]:
     repository, base = _get_gh_repository_base(repository)
-    print(f"repo={repository}")
     url = f"{base}/repos/{repository}/releases"
     response = _urlopen(url)
     return [result["tag_name"] for result in reversed(json.loads(response.read()))]
